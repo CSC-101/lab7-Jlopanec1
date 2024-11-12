dcfacfee8aac51c45b3cccce0744db17ea50548b
Jlopanec1 <jlopanec@calpoy.edu>
2024-11-11 20:27:12 -0800

    1st

--- convert_tests.py
+++ convert_tests.py
@@ -1,1 +1,10 @@
-
+#1
+def str_to_float(s) -> Optional[float]:
+    try:
+        s = float(s)
+        return s
+    except ValueError:
+        return None
+s = "34"
+print(str_to_float(s))
+print(type(float(s)))
